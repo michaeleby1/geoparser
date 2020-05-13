d8dd4098f816fed17bd769bf0eb47e9ea39ed4f4
Michael Eby <michaeleby1@gmail.com>
2020-05-13 14:20:57 -0400

    added Nominatum user_agent arg, wrapped cities into function

--- geoparser.py
+++ geoparser.py
@@ -11,7 +11,26 @@ import descartes
 import sys
 
 url = sys.argv[1]
-print(url)
 response = request.urlopen(url)
 raw = response.read().decode('utf-8')
-print(f'{type(raw)}, \n{len(raw)}, \n{raw[:501]}')
+# print(f'{type(raw)}, \n{len(raw)}, \n{raw[:501]}')
+
+# takes raw text and extracts city names
+def get_cities(raw):
+    places = GeoText(raw)
+    cities = list(places.cities)
+    return cities
+
+cities = get_cities(raw)
+
+geolocator = Nominatim(user_agent="geoparser", timeout=2)
+
+lat_lon = []
+for city in cities[:5]: 
+    try:
+        location = geolocator.geocode(city)
+        if location:
+            print(location.latitude, location.longitude)
+            lat_lon.append(location)
+    except GeocoderTimedOut as e:
+        print("Error: geocode failed on input %s with message %s"%(city, e))
