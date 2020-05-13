0bcfbae28909467fad38c3585f5c52b3f433ddf7
Michael Eby <michaeleby1@gmail.com>
2020-05-13 12:43:54 -0400

    imported shapely modules into script

---
+++ geoparsing.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+import pandas as pd
+import geopandas as gpd
+from urllib import request
+from geotext import GeoText
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+from shapely.geometry import Point, Polygon
+import descartes
+import sys
+
+url = sys.argv
